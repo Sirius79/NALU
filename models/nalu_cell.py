d76fe4ca7a6f4bb2928028e47c498d46230d64cc
Rajorshi Chaudhuri <rajorshi01@gmail.com>
2018-08-05 18:35:35 +0530

    Create nalu_cell.py

---
+++ models/nalu_cell.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.nn.parameter import Parameter
+import torch.nn.init as init
+import torch.nn.functional as F
+import numpy as np
+import math
+from nac import NAC
+
+class NeuralALUCell(nn.Module):
+  '''
+    Neural Arithmetic Logic Unit Cell 
+  '''
+  def __init__(self, input_size, output_size, epsilon = 1e-5):
+    '''
+      input_size: input dimension
+      output_size: output dimension
+      epsilon: prevents log0
+    '''
+    super(NeuralALUCell, self).__init__()
+    self.input = input_size
+    self.output = output_size
+    self.epsilon = epsilon
+    
+    self.G = Parameter(torch.Tensor(self.output, self.input, device=device))
+    self.W = Parameter(torch.Tensor(self.output, self.input, device=device))
+    self.register_parameter('bias', None)
+    self.nac = NAC(self.input, self.output)
+    
+    init.xavier_uniform_(self.G)
+    init.xavier_uniform_(self.W)
+  
+  def forward(self, input):
+    '''
+      g*a: addition-subtraction
+      (1-g)*m: multiplication-division
+    '''
+    a = self.nac(input)
+    g = F.sigmoid(F.linear(input, self.G, self.bias))
+    m = torch.exp(F.linear(torch.log(torch.abs(input) + self.epsilon)), self.W, self.bias)
+    return g*a + (1-g)*m
