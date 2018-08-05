6b4f5215261de5550449c6322ec71eee3751d807
Rajorshi Chaudhuri <rajorshi01@gmail.com>
2018-08-05 18:29:32 +0530

    Update nac.py

--- models/nac.py
+++ models/nac.py
@@ -1,1 +1,23 @@
-
+class NAC(nn.Module):
+  '''
+    Neural Architecture Cell
+  '''
+  def __init__(self, input_size, output_size):
+    '''
+      input_size: input dimension
+      output_size: output dimension
+    '''
+    super(NeuralArchitectureCell, self).__init__()
+    self.input = input_size
+    self.output = output_size
+    
+    self.W_h = Parameter(torch.Tensor(self.output, self.input, device=device))
+    self.M_h = Parameter(torch.Tensor(self.output, self.input, device=device))
+    self.W = Parameter(F.tanh(self.W_h) * F.sigmoid(self.M_h))
+    self.register_parameter('bias', None)
+    
+    init.xavier_uniform_(self.W_h)
+    init.xavier_uniform_(self.M_h)
+  
+  def forward(self, input):
+    return F.linear(input, self.W, self.bias)
